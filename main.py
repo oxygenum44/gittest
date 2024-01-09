93777cbb575c12abb6792bce5894f36ef42bcf2e
oxygenum44 <37884938+oxygenum44@users.noreply.github.com>
2024-01-09 12:14:35 +0100

    hey github CHANGE 2

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm2222')
     print_hi('hey github')
+    print_hi('hey github')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
