7edabb0a808010d4a2e803942d7614c657768926
oxygenum44 <37884938+oxygenum44@users.noreply.github.com>
2024-01-09 12:15:37 +0100

    hey github CHANGE 22343423325243552

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm2222')
+    print_hi('PyChar3432m2222')
     print_hi('hey gi3424543thub')
     print_hi('hey gith4353ub')
 
